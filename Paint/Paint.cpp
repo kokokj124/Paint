9d33ce0dfd2b83351e5b3192a1daeb64cbec0152
kokokj124 <49259557+kokokj124@users.noreply.github.com>
2020-04-29 18:14:01 +0700

    them pen

--- Paint/Paint.cpp
+++ Paint/Paint.cpp
@@ -53,9 +53,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     return (int) msg.wParam;
+
 }
-
-
 
 //
 //  FUNCTION: MyRegisterClass()
@@ -121,15 +120,25 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - post a quit message and return
 //
 //
-HWND hwndHinh[2], hwndMauNen[5], hwndMauVien[5], hwndGroup[3];
+HWND hwndHinh[3], hwndMauNen[5], hwndMauVien[5], hwndGroup[4], hwndCongCu[4];
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    static int xRight, yBottom, xLeft, yTop, Hinh;
+    static int xRight, yBottom, xLeft, yTop, Hinh = -1, penColor = 0;
     static HBRUSH hBrush[5], hbrushHinh;
     static HPEN hPen[5], hPenHinh;
-    COLORREF color[5] = { RGB(255,0,0),RGB(0,255,0),RGB(0,0,255),RGB(255,255,0),RGB(0,255,255) };
+    static int listHinh[] = { 131 , 132 , 133 ,154, 155, 156, 157};
+    static int hinh11 = 131;
+    DWORD  color[] = { RGB(64,   0,  64),	RGB(128,   0, 128),	RGB(128,   0,  64),	RGB(128, 128, 192),	RGB(255, 128, 192),
+    RGB(255, 128, 128),RGB(255, 128, 255),	RGB(255,   0, 255), RGB(255,   0, 128),	RGB(128,   0, 255), RGB(64,   0, 128),
+    RGB(255, 255, 128),	RGB(255, 255,   0),	RGB(255, 128,  64),	RGB(255, 128,   0),	RGB(128,  64,   0),	RGB(128, 128,   0),
+    RGB(0,   0,   0),	RGB(64,   0,   0),	RGB(128,   0,   0),	RGB(128,  64,  64),	RGB(255,   0,   0),  RGB(255, 255, 255),
+    RGB(128, 128,  64),	RGB(0,  64,   0),	RGB(0, 128,   0),	RGB(0, 255,   0),	RGB(128, 255,   0),	RGB(128, 255, 128),
+    RGB(0, 255, 128),	RGB(0, 255,  64),	RGB(0, 128, 128),	RGB(0, 128,  64),	RGB(0,  64,  64),	RGB(128, 128, 128),
+    RGB(64, 128, 128),	RGB(0,   0, 128),	RGB(0,   0, 255),	RGB(0,  64, 128),	RGB(0, 255, 255), RGB(128, 255, 255),
+    RGB(0, 128, 255),	RGB(0, 128, 192),	RGB(128, 128, 255),	RGB(0,   0, 160),	RGB(0,   0,  64),	RGB(192, 192, 192),
+    RGB(64,   0,  64),};
     HDC hdc; // Bien De Lay Handle Cua Ngu Canh Thiet Bi (DC-Divice Context)
-
+    static bool flag = false;
     switch (message)
     {
     case WM_CREATE:
@@ -137,11 +146,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         hwndGroup[0] = CreateWindow(TEXT("button"), TEXT("Chọn hình"),
             WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 3, 3, 130, 80, hWnd, (HMENU)20, NULL, NULL);
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
             hwndHinh[i] = CreateWindow(TEXT("button"), TEXT(""),
-                WS_CHILD | WS_VISIBLE | BS_BITMAP, 5 + i * 50 + 8, 25, 50, 50, hWnd, (HMENU)i, NULL, NULL);
-            HANDLE  hbitmap = LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(131), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
-            SendMessage(hwndHinh[i], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbitmap); 
+               WS_CHILD | WS_VISIBLE | BS_BITMAP, 2 + i * 40 + 8, 25, 40, 40, hWnd, (HMENU)i, NULL, NULL);
+            HANDLE  hbitmap = LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(listHinh[i]), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+            SendMessage(hwndHinh[i], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbitmap);
         }
 
         hwndGroup[1] = CreateWindow(TEXT("button"), TEXT("Chọn màu nền"),
@@ -150,17 +159,45 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 450, 3, 300, 80, hWnd, (HMENU)22, NULL, NULL);
         for (int i = 0; i < 5; i++)
         {
-            hwndMauNen[i] = CreateWindow(TEXT("button"), TEXT(""),
-                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 143 + i * 50 + 8, 25, 50, 50, hWnd, (HMENU)(i + 2), NULL, NULL);
-            hwndMauVien[i] = CreateWindow(TEXT("button"), TEXT(""),
-                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 453 + i * 50 + 8, 25, 50, 50, hWnd, (HMENU)(i + 7), NULL, NULL);
+            hwndMauNen[i] = CreateWindow(TEXT("button"), TEXT(""), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 143 + i * 50 + 8 , 25, 50, 50, hWnd, HMENU(i + 3) , NULL, NULL);
+            hwndMauVien[i] = CreateWindow(TEXT("button"), TEXT("") ,WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 453 + i * 50 + 8 , 25, 50, 50, hWnd, HMENU(i + 8) , NULL, NULL);
             hBrush[i] = CreateSolidBrush(color[i]);
+        }
+
+        hwndGroup[3] = CreateWindow(TEXT("button"), TEXT("Chọn bút vẽ"),
+            WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 760, 3, 220, 80, hWnd, (HMENU)23, NULL, NULL);
+        for (int i = 0; i < 4; i++) {
+            hwndCongCu[i] = CreateWindow(TEXT("button"), TEXT(""),
+                WS_CHILD | WS_VISIBLE | BS_BITMAP, 760 + i * 40 + 8, 25, 40, 40, hWnd, (HMENU)(i+13), NULL, NULL);
+            HANDLE  hbitmap = LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(listHinh[i + 3]), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+            SendMessage(hwndCongCu[i], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbitmap);
         }
         break;
 
     }
+
+    case WM_MOUSEMOVE:
+    {
+        if (flag == true) {
+            hdc = GetDC(hWnd);
+            int x = LOWORD(lParam);
+            int y = HIWORD(lParam);
+            for (int i = 1; i < 6; i++) {
+                for (int j = 1; j < 6; j++) {
+                    SetPixel(hdc, x + i, y + j,color[penColor]);
+                }
+            }
+            ReleaseDC(hWnd, hdc);
+        }
+        break;
+    }
     case WM_LBUTTONDOWN:
         {
+            if (Hinh == 13)
+            {
+                if (flag == true) flag = false;
+                else flag = true;
+            }
             xLeft = LOWORD(lParam);
             yTop = HIWORD(lParam);
             break;
@@ -170,6 +207,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             xRight = LOWORD(lParam);
             yBottom = HIWORD(lParam);
             hdc = GetDC(hWnd);
+            SelectObject(hdc, hPenHinh);
+            SelectObject(hdc, hbrushHinh);
+            if(Hinh == 0) Rectangle(hdc, xLeft, yTop, xRight, yBottom);
+            else if(Hinh == 1) Ellipse(hdc, xLeft, yTop, xRight, yBottom);
+            else if (Hinh == 2) {
+                POINT apt[] = { {xLeft, yBottom}, {xLeft, yTop}, {xRight, yBottom}, {xLeft, yBottom}, {xRight, yTop }, {xRight,yBottom}};
+                Polygon(hdc, apt,6);
+            }
+            ReleaseDC(hWnd, hdc);
             break;
         }
     case WM_COMMAND:
@@ -180,30 +226,69 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             {
             case 0:
                 Hinh = 0;
-                //InvalidateRect(hwndHinh[0], NULL, FALSE);
+                InvalidateRect(hwndHinh[0], NULL, FALSE);
                 break;
             case 1:
                 Hinh = 1;
                 InvalidateRect(hwndHinh[1], NULL, FALSE);// Khai Bao Cua So Can Ve Lai
                 break;
             case 2:
+                Hinh = 2;
+                InvalidateRect(hwndHinh[2], NULL, FALSE);
+            case 3:
                 hbrushHinh = hBrush[0];
                 InvalidateRect(hwndMauNen[0], NULL, FALSE);
                 break;
-            case 3:
+            case 4:
                 hbrushHinh = hBrush[1];
                 InvalidateRect(hwndMauNen[1], NULL, FALSE);
                 break;
-                //Tương tự cho 4,5,6
+            case 5:
+                hbrushHinh = hBrush[2];
+                InvalidateRect(hwndMauNen[2], NULL, FALSE);
+                break;
+            case 6:
+                hbrushHinh = hBrush[3];
+                InvalidateRect(hwndMauNen[3], NULL, FALSE);
+                break;
             case 7:
-                hPenHinh = CreatePen(PS_SOLID, 1, color[0]);
-                InvalidateRect(hwndMauVien[0], NULL, FALSE);
+                hbrushHinh = hBrush[4];
+                InvalidateRect(hwndMauNen[4], NULL, FALSE);
                 break;
             case 8:
+                hPenHinh = CreatePen(PS_SOLID, 1, color[0]);
+                penColor = 0;
+                InvalidateRect(hwndMauVien[0], NULL, FALSE);
+                break;
+            case 9:
                 hPenHinh = CreatePen(PS_SOLID, 1, color[1]);
+                penColor = 1;
                 InvalidateRect(hwndMauVien[1], NULL, FALSE);
                 break;
-
+            case 10:
+                hPenHinh = CreatePen(PS_SOLID, 1, color[2]);
+                penColor = 2;
+                InvalidateRect(hwndMauVien[2], NULL, FALSE);
+                break;
+            case 11:
+                hPenHinh = CreatePen(PS_SOLID, 1, color[3]);
+                penColor = 3;
+                InvalidateRect(hwndMauVien[3], NULL, FALSE);
+                break;
+            case 12:
+                hPenHinh = CreatePen(PS_SOLID, 1, color[4]);
+                penColor = 4;
+                InvalidateRect(hwndMauVien[4], NULL, FALSE);
+                break;
+            case 13:
+                Hinh = 13;
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
--- Paint/resource.h
+++ Paint/resource.h
@@ -14,6 +14,12 @@
 #define IDC_PAINT                       109
 #define IDR_MAINFRAME                   128
 #define IDB_HinhChuNhat                 131
+#define IDB_HinhElip                    132
+#define IDB_HinhDaGiac                  133
+#define IDB_BITMAP1                     154
+#define IDB_BITMAP2                     155
+#define IDB_BITMAP3                     156
+#define IDB_BITMAP4                     157
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -21,9 +27,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        158
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
