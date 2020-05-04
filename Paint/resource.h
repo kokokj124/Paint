33a36b44bf2c143900bdb4e08adc466811621667
kokokj124 <49259557+kokokj124@users.noreply.github.com>
2020-05-04 21:02:52 +0700

    paint thieu text

--- Paint/resource.h
+++ Paint/resource.h
@@ -4,7 +4,6 @@
 //
 #define ID_NAM                          1
 #define IDC_MYICON                      2
-#define IDD_PAINT_DIALOG                102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
@@ -20,6 +19,29 @@
 #define IDB_BITMAP2                     155
 #define IDB_BITMAP3                     156
 #define IDB_BITMAP4                     157
+#define IDC_CURSOR1                     158
+#define IDB_BITMAP5                     159
+#define IDD_DIALOG2                     161
+#define IDD_DIALOG1                     162
+#define IDB_BITMAP6                     163
+#define IDB_BITMAP7                     164
+#define IDB_BITMAP8                     165
+#define IDB_BITMAP9                     166
+#define IDB_BITMAP10                    167
+#define IDB_BITMAP11                    168
+#define IDC_NetLien                     1000
+#define IDC_NetDut                      1002
+#define IDC_FontText                    1004
+#define IDC_B                           1005
+#define IDC_I                           1006
+#define IDC_U                           1007
+#define IDC_TextSize                    1008
+#define ID_Edit                         1009
+#define IDC_OK11                        1019
+#define IDC_NetCham                     1020
+#define IDC_NetNgacNgangVaDauKep        1021
+#define IDC_NetChamVaNgachNgang         1022
+#define IDC_OK                          1024
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -27,9 +49,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        158
+#define _APS_NEXT_RESOURCE_VALUE        169
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
